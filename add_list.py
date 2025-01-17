8e298cc2d9f11b12ca842e2eccacb37c7e7f5136
Ksaco <92227378+Ksaco@users.noreply.github.com>
2025-01-17 15:47:09 +0700

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,9 @@
+main_themes = input("Введите основные темы: ")
+characters = input("Введите персонажи: ")
+reading_recommendations = input("Введите рекомендации для чтения: ")
+data_list = [main_themes, characters, reading_recommendations]
+
+print("Основные темы:", main_themes)
+print("Персонажи:", characters)
+print("Рекомендации для чтения:", reading_recommendations)
+print("Список всех переменных:", data_list)
